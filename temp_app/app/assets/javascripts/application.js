1178e48a5095c0727d942bde6563964dbffec638
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-07-07 10:50:38 -0400

    searchable zipcodes done

--- temp_app/app/assets/javascripts/application.js
+++ temp_app/app/assets/javascripts/application.js
@@ -16,23 +16,27 @@
 //= require_tree .
 
 $(document).ready(function(){
+
 	var form = $("#search")
 	var tempSearch = $("#temp-search")
+
 	form.on("submit", function(event){
  	event.preventDefault()
-	var filters = [{query: "20052"}]
-	var weatherOnline = "http://api.worldweatheronline.com/free/v2/weather.ashx?" + filters + "&format=json&date=today&includelocation=yes&key=a0a0205a5848630b4b3cb200e4683"
+	var query = tempSearch.val()
 	$.ajax({
-		url: weatherOnline,
-    	data: {"q": JSON.stringify({"filters": filters})},
+		url: "http://api.worldweatheronline.com/free/v2/weather.ashx?" + query + "&format=json&date=today&includelocation=yes&key=a0a0205a5848630b4b3cb200e4683",
+    	data: {"q": JSON.stringify({"query": query})},
     	type: "GET",
-    	dataType: "jsonp"
+    	dataType: "jsonp",
+    	context: this
   	}).done(function(response){
   		$("#location").append("   " + response.data.nearest_area[0].areaName[0].value + "")
 
 	  	$("#temperature_c").append("   " + response.data.current_condition[0].temp_C + " Celsius")
 
     	$("#temperature_f").append("   " + response.data.current_condition[0].temp_F + " Fahreneit")
-		})
-  	});
+    	
+	});
+  });
+
 });
